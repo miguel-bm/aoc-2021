c8703b72a7a4910e0e8048a82917708812311a7a
miguel-bm <miguel.blanco.marcos@gmail.com>
2021-12-07 09:56:01 +0100

    fix(template): Minor fixes to template files

--- utils/template/solution.py
+++ utils/template/solution.py
@@ -26,18 +26,18 @@ if __name__ == "__main__":
     data = parse_input(raw_data)
 
     if example:
-        example = parse_input(example)
+        example_data = parse_input(example)
 
     # Part 1
     if example:
-        assert example_sol_a == solve_a(example)
+        assert example_sol_a == solve_a(example_data)
     solution_a = solve_a(data)
     print(solution_a)
     puzzle.answer_a = solution_a
 
     # Part 2
     if example:
-        assert example_sol_b == solve_b(example)
+        assert example_sol_b == solve_b(example_data)
     solution_b = solve_b(data)
     print(solution_b)
     puzzle.answer_b = solution_b
